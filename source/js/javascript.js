320eacc9883cbea3694c2485b6aee800848f279d
Irina Lukyanova <runnaf@mail.ru>
2021-12-19 08:32:45 +0300

    исправила открывающееся меню

--- source/js/javascript.js
+++ source/js/javascript.js
@@ -1,15 +1,51 @@
-var navMain = document.querySelector('.country-nav');
-  var navToggle = document.querySelector('.country-nav__toggle');
+var navMain = document.querySelector('.main-nav');
+  var navToggle = document.querySelector('.page-header__toggle');
 
-  navMain.classList.remove('country-nav--nojs');
+  navMain.classList.remove('main-nav--nojs');
 
   navToggle.addEventListener('click', function() {
-    if (navMain.classList.contains('country-nav--closed')) {
-      navMain.classList.remove('country-nav--closed');
-      navMain.classList.add('country-nav--opened');
+    if (navMain.classList.contains('main-nav--closed')) {
+      navMain.classList.remove('main-nav--closed');
+      navMain.classList.add('main-nav--opened');
     } else {
-      navMain.classList.add('country-nav--closed');
-      navMain.classList.remove('country-nav--opened');
+      navMain.classList.add('main-nav--closed');
+      navMain.classList.remove('main-nav--opened');
     }
   });
 
+  var navToggle = document.querySelector('.page-header__toggle');
+
+  navToggle.classList.remove('page-header__toggle--nojs');
+
+  navToggle.addEventListener('click', function () {
+    if (navToggle.classList.contains('page-header__toggle-closed')) {
+      navToggle.classList.remove('page-header__toggle-closed');
+      navToggle.classList.add('page-header__toggle-opened');
+    } else {
+      navToggle.classList.add('page-header__toggle-closed');
+      navToggle.classList.remove('page-header__toggle-opened');
+    }
+  });
+
+  var navHeader = document.querySelector('.page-header');
+  var navToggle = document.querySelector('.page-header__toggle');
+
+  navHeader.classList.remove('page-header--nojs');
+
+  navToggle.addEventListener('click', function () {
+    if (navHeader.classList.contains('page-heder--closed')) {
+      navHeader.classList.remove('page-header--closed');
+      navHeader.classList.add('page-header--opened');
+    } else {
+      navHeader.classList.add('page-header--closed');
+      navHeader.classList.remove('page-header--opened');
+    }
+  });
+
+
+let navToggle = document.querySelector('.page-header__toggle');
+
+navToggle.onclick = function() {
+  page.classList.toggle('page-header__toggle-closed');
+  page.classList.toggle('page-header__toggle-opened');
+};
